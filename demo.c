1d86e1e9424023c880cb3465b5dc705e778f0cd6
6704062610160 <s6704062610160@email.kmutnb.ac.th>
2025-12-03 00:32:41 +0700

    Add files via upload

--- demo.c
+++ demo.c
@@ -2,19 +2,21 @@
 
 int main(){
 	float a,b,c,d,e,f;
-	a = 75;
-	b = 80;
-	c = 40;
-	d = 45;
-	e = 16;
-	f = 15;
+	scanf("%f %f %f %f %f %f",&a,&b,&c,&d,&e,&f);
 	
-
-	printf("%10s%10.0f\n","Mid=",a);
-	printf("%10s%10.0f\n","Final=",b);
-	printf("%10s%10.0f\n","Q1=",c);
-	printf("%10s%10.0f\n","Q2=",d);
-	printf("%10s%10.0f\n","lab=",e);
-	printf("%10s%10.0f\n","sit=",f);
+	float s1 = (a/100)*30;
+	float s2 = (b/120)*35;
+	float s3 = (c/60)*12;
+	float s4 = (d/60)*13;
+	float s5 = (e/16)*5;
+	float s6 = (f/16)*5;
+	printf("%10s%10.0f => %5.2f\n","Mid=",a,s1);
+	printf("%10s%10.0f => %5.2f\n","Final=",b,s2);
+	printf("%10s%10.0f => %5.2f\n","Q1=",c,s3);
+	printf("%10s%10.0f => %5.2f\n","Q2=",d,s4);
+	printf("%10s%10.0f => %5.2f\n","lab=",e,s5);
+	printf("%10s%10.0f => %5.2f\n","sit=",f,s6);
 	
+	float sum = s1 + s2 + s3 + s4 + s5 + s6;
+	printf("*****%.2f******\n",sum);
 }
