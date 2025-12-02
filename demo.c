ffc7c43ccac6a940e082e04e6a7ffb2470e90f5d
6704062610160 <s6704062610160@email.kmutnb.ac.th>
2025-12-03 00:02:51 +0700

    Add files via upload

---
+++ demo.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+	float a,b,c,d,e,f;
+	a = 75;
+	b = 80;
+	c = 40;
+	d = 45;
+	e = 16;
+	f = 15;
+	
+
+	printf("Mid=%10.0f\n",a);
+	printf("Final=%10.0f\n",b);
+	printf("Q1=%10.0f\n",c);
+	printf("Q2=%10.0f\n",d);
+	printf("lab=%10.0f\n",e);
+	printf("sit=%10.0f\n",f);
+	
+}
